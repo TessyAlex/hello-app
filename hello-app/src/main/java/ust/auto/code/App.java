1fbd08ebcd492e7734bdb3676fef52f8913b0d0b
TessyAlex <245208@ust.com>
2023-05-25 16:52:08 +0530

    first source commit

---
+++ hello-app/src/main/java/ust/auto/code/App.java
@@ -0,0 +1,13 @@
+package ust.auto.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
